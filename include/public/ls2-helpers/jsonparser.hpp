db81bc1d7599609d8ce3c1e1b0e35a1ed442eade
nguyenhai.duong <nguyenhai.duong@lgepartner.com>
2023-09-05 20:03:48 +0900

    Fix JSON parser constructor
    :Release Notes:
    Add missing member initialization in JSON parser constructor.
    
    :Detailed Notes:
    The constructor of the JSON parser was missing initialization of
    the `_valueNull` member. This patch adds `_valueNull` to the list
    of initialized members in the constructor, ensuring all members
    are correctly initialized from the provided context.
    
    :Testing Performed:
    Test cases verified: QWTC-9080, QWTC-10388, QWTC-8560
    
    :QA Notes:
    N/A
    
    :Issues Addressed:
    [WRR-18303] Patch merge for ls2-helper in repo
    
    Change-Id: Ie480ba5f94b74b6ef6564d50073be232d7ebdd4a

--- include/public/ls2-helpers/jsonparser.hpp
+++ include/public/ls2-helpers/jsonparser.hpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2018 LG Electronics, Inc.
+// Copyright (c) 2016-2025 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -449,6 +449,7 @@ public:
 			_fieldName(context._fieldName),
 			_destination(context._destination),
 			_valueRead(context._valueRead),
+			_valueNull(context._valueNull),
 			_optional(context._optional),
 			_allowNull(context._allowNull)
 	{
