06dc18592d0bbed0f565947bf1f7f176eb0ceff3
vinod.sharma <vinod.sharma@lge.com>
2024-05-17 11:50:21 +0530

    ls2-helper: Static issue
    :Release Notes:
    Fix to ls2-helper static issue.
    
    :Detailed Notes:
    coverity fix for below CIDs in ls2-helper
    9047198
    
    :Testing Performed:
    Locally Tested.
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [WRQ-20662] Missing checking NULL for pointer
    
    Change-Id: I795cc4dfa5ff3289696cfe5eb6933c643151bb3c

--- src/subscriptionpoint.cpp
+++ src/subscriptionpoint.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2016-2018 LG Electronics, Inc.
+// Copyright (c) 2016-2024 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -78,6 +78,10 @@ bool SubscriptionPoint::postSubscriptions(gpointer user_data)
 // subscriptions.
 bool SubscriptionPoint::post(const char *payload) noexcept
 {
+	if (!payload) {
+		LOG_LS_DEBUG("payload as NULL. [%s]\n", __func__);
+		return false;
+	}
 	if (!mServiceHandle)
 		return false;
 
